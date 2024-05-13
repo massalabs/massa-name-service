bb6981ffa60bf7f0ddaafcd7eb748096cc8f9301
AurelienFT <aurelien.foucault@epitech.eu>
2024-05-14 00:05:52 +0200

    start interaction sc claim

--- frontend/src/components/MNSClaim.tsx
+++ frontend/src/components/MNSClaim.tsx
@@ -1,20 +1,49 @@
-import { Button } from '@massalabs/react-ui-kit';
+import { Button, toast } from '@massalabs/react-ui-kit';
 import { InputWithRightText } from './InputWithRightText';
+import { useState } from 'react';
+import { useWriteMNS } from '../utils/write-mns-sc';
+import { useAccountStore } from '../lib/connectMassaWallets/store';
 
 export function MNSClaim() {
-  return (
-    <div>
-      <div className="grid grid-cols-4 gap-2">
-        <div className="col-span-3">
-          <InputWithRightText
-            customClass="w-96 border-none"
-            rightText=".massa"
-            placeholder="Enter a domain"
-          />
-          <p className="mb-4 font-light text-neutral">Price 0.0000 MAS</p>
+    const [domain, setDomain] = useState<string>('');
+    const { connectedAccount, massaClient } = useAccountStore();
+    const { dnsAlloc, getAllocCost } = useWriteMNS(massaClient);
+    const [price, setPrice] = useState<number>(0);
+
+    function claim() {
+        if (!connectedAccount) {
+            toast.error('Please connect your wallet');
+            return;
+        }
+        dnsAlloc({ domain, targetAddress: connectedAccount.address() });
+    }
+
+    function onDomainChange(domain: string) {
+        if (!connectedAccount) {
+            toast.error('Please connect your wallet');
+            return;
+        }
+        setDomain(domain);
+        getAllocCost({ domain, targetAddress: connectedAccount?.address() ?? '' }).then((cost) => {
+            setPrice(cost);
+        });
+    }
+    return (
+        <div>
+            <div className="grid grid-cols-4 gap-2">
+                <div className="col-span-3">
+                    <InputWithRightText
+                        customClass="w-96 border-none"
+                        rightText=".massa"
+                        placeholder="Enter a domain"
+                        onChange={(e) => { onDomainChange(e.target.value) }}
+                    />
+                    <p className="mb-4 font-light text-neutral">Price  MAS</p>
+                </div>
+                <Button
+                    onClick={() => claim()}
+                >Claim</Button>
+            </div>
         </div>
-        <Button>Claim</Button>
-      </div>
-    </div>
-  );
+    );
 }
---
+++ frontend/src/utils/write-mns-sc.tsx
@@ -0,0 +1,198 @@
+import { Args, Client, EOperationStatus, ICallData, MAX_GAS_CALL, fromMAS } from '@massalabs/massa-web3';
+import { ToastContent, toast } from '@massalabs/react-ui-kit';
+import { useState } from 'react';
+import { DEFAULT_OP_FEES, SC_ADDRESS } from '../const/sc';
+import { OperationToast } from '../lib/connectMassaWallets/components/OperationToast';
+import { logSmartContractEvents } from '../lib/connectMassaWallets/utils';
+
+interface ToasterMessage {
+    pending: string;
+    success: string;
+    error: string;
+    timeout?: string;
+}
+
+interface DnsAllocParams {
+    domain: string;
+    targetAddress: string;
+}
+
+type callSmartContractOptions = {
+    coins?: bigint;
+    fee?: bigint;
+    showInProgressToast?: boolean;
+};
+
+function minBigInt(a: bigint, b: bigint) {
+    return a < b ? a : b;
+}
+export function useWriteMNS(client?: Client) {
+    const [isPending, setIsPending] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [opId, setOpId] = useState<string | undefined>(undefined);
+
+    async function getAllocCost(params: DnsAllocParams): Promise<number> {
+        let args = new Args();
+        args.addString(params.domain);
+        args.addString(params.targetAddress);
+        let response = await client?.smartContracts().readSmartContract({
+            targetAddress: SC_ADDRESS,
+            targetFunction: 'dnsAlloc',
+            parameter: args.serialize(),
+            fee: fromMAS(100000),
+        });
+        if (!response) {
+            return 0;
+        }
+        return response.info.gas_cost;
+    }
+
+    function callSmartContract(
+        targetFunction: string,
+        parameter: number[],
+        messages: ToasterMessage,
+        {
+            coins = BigInt(0),
+            fee = DEFAULT_OP_FEES,
+            showInProgressToast = false,
+        }: callSmartContractOptions = {},
+    ) {
+        if (!client) {
+            throw new Error('Massa client not found');
+        }
+        if (isPending) {
+            throw new Error('Operation is already pending');
+        }
+        setIsSuccess(false);
+        setIsError(false);
+        setIsPending(false);
+        let operationId: string | undefined;
+        let toastId: string | undefined;
+
+        const callData = {
+            targetAddress: SC_ADDRESS,
+            targetFunction,
+            parameter,
+            coins,
+            fee,
+        } as ICallData;
+
+        client
+            .smartContracts()
+            .readSmartContract(callData)
+            .then((response) => {
+                const gasCost = BigInt(response.info.gas_cost);
+                return minBigInt(gasCost + (gasCost * 20n) / 100n, MAX_GAS_CALL);
+            })
+            .then((maxGas: bigint) => {
+                callData.maxGas = maxGas;
+                return client.smartContracts().callSmartContract(callData);
+            })
+            .then((opId) => {
+                operationId = opId;
+                setOpId(opId);
+                setIsPending(true);
+                if (showInProgressToast) {
+                    toastId = toast.loading(
+                        (t) => (
+                            <ToastContent t={t}>
+                                <OperationToast
+                                    title={messages.pending}
+                                    operationId={operationId}
+                                />
+                            </ToastContent>
+                        ),
+                        {
+                            duration: Infinity,
+                        },
+                    );
+                }
+                return client
+                    .smartContracts()
+                    .awaitMultipleRequiredOperationStatus(opId, [
+                        EOperationStatus.SPECULATIVE_ERROR,
+                        EOperationStatus.FINAL_ERROR,
+                        EOperationStatus.FINAL_SUCCESS,
+                    ]);
+            })
+            .then((status: EOperationStatus) => {
+                if (status !== EOperationStatus.FINAL_SUCCESS) {
+                    throw new Error('Operation failed', { cause: { status } });
+                }
+                setIsSuccess(true);
+                setIsPending(false);
+                toast.dismiss(toastId);
+                toast.success((t) => (
+                    <ToastContent t={t}>
+                        <OperationToast
+                            title={messages.success}
+                            operationId={operationId}
+                        />
+                    </ToastContent>
+                ));
+            })
+            .catch((error) => {
+                console.error(error);
+                toast.dismiss(toastId);
+                setIsError(true);
+                setIsPending(false);
+
+                if (!operationId) {
+                    console.error('Operation ID not found');
+                    toast.error((t) => (
+                        <ToastContent t={t}>
+                            <OperationToast title={messages.error} />
+                        </ToastContent>
+                    ));
+                    return;
+                }
+
+                if (
+                    [
+                        EOperationStatus.FINAL_ERROR,
+                        EOperationStatus.SPECULATIVE_ERROR,
+                    ].includes(error.cause?.status)
+                ) {
+                    toast.error((t) => (
+                        <ToastContent t={t}>
+                            <OperationToast
+                                title={messages.error}
+                                operationId={operationId}
+                            />
+                        </ToastContent>
+                    ));
+                    logSmartContractEvents(client, operationId);
+                } else {
+                    toast.error((t) => (
+                        <ToastContent t={t}>
+                            <OperationToast
+                                title={messages.timeout || "Operation timeout"}
+                                operationId={operationId}
+                            />
+                        </ToastContent>
+                    ));
+                }
+            });
+    }
+
+    function dnsAlloc(params: DnsAllocParams) {
+        let args = new Args();
+        args.addString(params.domain);
+        args.addString(params.targetAddress);
+        callSmartContract('dnsAlloc', args.serialize(), {
+            pending: "Claiming...",
+            success: "Successfully claimed",
+            error: "Failed to claim",
+        });
+    }
+
+    return {
+        opId,
+        isPending,
+        isSuccess,
+        isError,
+        dnsAlloc,
+        getAllocCost
+      };
+}
