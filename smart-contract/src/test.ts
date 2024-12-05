faf01258a094262ff162b656dc4b48d8209ff125
Peterjah <ps@massa.net>
2024-12-05 13:45:26 +0100

    update massaweb3 in sc script

--- smart-contract/src/test.ts
+++ smart-contract/src/test.ts
@@ -1,71 +1,29 @@
-import * as dotenv from 'dotenv';
-import path from 'path';
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { getEnvVariable } from './utils';
-import { deploySC, WalletClient, ISCData } from '@massalabs/massa-sc-deployer';
-import {
-  Args,
-  fromMAS,
-  MAX_GAS_DEPLOYMENT,
-  CHAIN_ID,
-  ClientFactory,
-  DefaultProviderUrls,
-  EOperationStatus,
-  bytesToStr,
-} from '@massalabs/massa-web3';
+import { Account, Web3Provider, MNS } from '@massalabs/massa-web3';
+import { config } from 'dotenv';
+config();
 
-// Load .env file content into process.env
-dotenv.config();
+const MNS_CONTRACT = 'AS1qyye6EfPSvj8whjEeUCZepD3mnhYvaaLub65DGcF6jdxydBPs';
 
-// Get environment variables
-const publicApi = getEnvVariable('JSON_RPC_URL_PUBLIC');
-const secretKey = getEnvVariable('WALLET_SECRET_KEY');
-// Define deployment parameters
-const chainId = CHAIN_ID.BuildNet; // Choose the chain ID corresponding to the network you want to deploy to
+const account = await Account.fromEnv();
+const provider = Web3Provider.buildnet(account);
+const MNSContract = new MNS(provider, MNS_CONTRACT);
 
-const testAccount = await WalletClient.getAccountFromSecretKey(secretKey);
-if (!testAccount || !testAccount.address) {
-  throw new Error('Account not found');
-}
-const client = await ClientFactory.createDefaultClient(
-  DefaultProviderUrls.BUILDNET,
-  chainId,
-  true,
-  testAccount,
-);
 // Claim dns
-const domain = 'testaurelien';
-const opId = await client.smartContracts().callSmartContract({
-  coins: fromMAS(10),
-  targetAddress: 'AS1MbHVdYjMLNshxF7koLkstXbX7RGJoN2bqb2BjTK3WAQEWpECX',
-  targetFunction: 'dnsAlloc',
-  parameter: new Args().addString(domain).addString(testAccount.address),
-  fee: fromMAS(0.01),
+const domain = 'testaurelienlol';
+
+const op = await MNSContract.alloc(domain, provider.address, {
+  coins: BigInt(1043000000),
 });
-const status = await client
-  .smartContracts()
-  .awaitMultipleRequiredOperationStatus(opId, [
-    EOperationStatus.SPECULATIVE_SUCCESS,
-    EOperationStatus.SPECULATIVE_ERROR,
-  ]);
-if (!(status === EOperationStatus.SPECULATIVE_SUCCESS)) {
-  const error = await client.smartContracts().getFilteredScOutputEvents({
-    start: null,
-    end: null,
-    original_operation_id: opId,
-    original_caller_address: null,
-    emitter_address: null,
-    is_final: null,
-  });
+
+const events = await op.getSpeculativeEvents();
+if (events.length) {
+  console.log(events.map((e) => e.data));
 }
 
-const addressFetched = bytesToStr(
-  (
-    await client.smartContracts().readSmartContract({
-      targetAddress: 'AS1MbHVdYjMLNshxF7koLkstXbX7RGJoN2bqb2BjTK3WAQEWpECX',
-      targetFunction: 'dnsResolve',
-      parameter: new Args().addString(domain),
-    })
-  ).returnValue,
-);
+const domainOwner = await MNSContract.resolve(domain);
+
+if (domainOwner === provider.address) {
+  console.log('OK! Domain is owned by the current account');
+} else {
+  console.error('KO! Domain is not owned by the current account');
+}
