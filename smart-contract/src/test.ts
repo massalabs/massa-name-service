94a81c741a8e25b60859dcdd804da4b4ab9a5794
Peterjah <ps@massa.net>
2024-12-17 10:52:39 +0100

    add helpers scripts

--- smart-contract/src/test.ts
+++ smart-contract/src/test.ts
@@ -1,18 +1,16 @@
-import { Account, Web3Provider, MNS } from '@massalabs/massa-web3';
-import { config } from 'dotenv';
-config();
+import { MNS, strToBytes, bytesToStr } from '@massalabs/massa-web3';
+import { DOMAIN_SEPARATOR_KEY, initProvider } from './utils';
+import { MNS_CONTRACT } from './config';
 
-const MNS_CONTRACT = 'AS1qyye6EfPSvj8whjEeUCZepD3mnhYvaaLub65DGcF6jdxydBPs';
+const provider = await initProvider();
 
-const account = await Account.fromEnv();
-const provider = Web3Provider.buildnet(account);
 const MNSContract = new MNS(provider, MNS_CONTRACT);
 
 // Claim dns
-const domain = 'testaurelienlol';
+const domain = 'testaurelienlolloulilolsdsdssdsd';
 
 const op = await MNSContract.alloc(domain, provider.address, {
-  coins: BigInt(1043000000),
+  coins: BigInt(2000000000),
 });
 
 const events = await op.getSpeculativeEvents();
@@ -26,4 +24,37 @@ if (domainOwner === provider.address) {
   console.log('OK! Domain is owned by the current account');
 } else {
   console.error('KO! Domain is not owned by the current account');
+}
+
+// get owned tokens
+const OWNED_TOKENS_KEY = strToBytes('ownedTokens');
+const OWNED_FILTER = Uint8Array.from([
+  ...OWNED_TOKENS_KEY,
+  ...strToBytes(provider.address),
+]);
+const ownedkeys = await provider.getStorageKeys(
+  MNS_CONTRACT,
+  OWNED_FILTER,
+  false,
+);
+console.log('Nb owned tokens:', ownedkeys.length);
+const domainKeys = ownedkeys.map((k) => {
+  const tokenIdBytes = k.slice(OWNED_FILTER.length);
+  const DOMAIN_KEY_PREFIX = [0x03];
+
+  return Uint8Array.from([
+    ...DOMAIN_SEPARATOR_KEY,
+    ...DOMAIN_KEY_PREFIX,
+    ...tokenIdBytes,
+  ]);
+});
+const domainsBytes = await provider.readStorage(
+  MNS_CONTRACT,
+  domainKeys,
+  false,
+);
+const domains = domainsBytes.map((d) => bytesToStr(d));
+console.log('Owned domains:', domains);
+if (!domains.includes(domain)) {
+  console.error('KO! Domain is not in the owned domains');
 }
